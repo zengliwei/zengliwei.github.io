3924f4849a7444a440bd92adb3d412a06b7eeee6
zengliwei <zengliwei@163.com>
2021-09-10 18:44:25 +0800

    update

--- web/js/require-config.js
+++ web/js/require-config.js
@@ -3,9 +3,9 @@ require.config({
     paths: {
         jquery: 'web/js/lib/jquery',
         text: 'web/js/lib/text',
+        knockout: 'web/js/lib/knockout',
         mousewheel: 'web/js/lib/jquery.mousewheel-3.0.6.min',
         highlight: 'web/js/lib/highlight.min',
-        popup: 'web/js/lib/jquery.fancybox.min',
         scrollbar: 'web/js/lib/jquery.mCustomScrollbar.min'
     },
     shim: {
